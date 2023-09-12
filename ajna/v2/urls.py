9d8a781ca7941546e804d913a514d61cf7814747
Aljosa Urbas <aljosaurbas@gmail.com>
2023-09-12 13:40:11 +0200

    feat: added BucketsView, BucketsGraphView to v2

--- ajna/v2/urls.py
+++ ajna/v2/urls.py
@@ -13,4 +13,14 @@ urlpatterns = [
         pool.PoolView.as_view(),
         name="pool",
     ),
+    path(
+        "pools/<pool_address>/buckets/",
+        pool.BucketsView.as_view(),
+        name="pool-buckets",
+    ),
+    path(
+        "pools/<pool_address>/buckets/graph/",
+        pool.BucketsGraphView.as_view(),
+        name="pool-buckets-graph",
+    ),
 ]
