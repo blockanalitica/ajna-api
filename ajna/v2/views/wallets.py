9fff9c17474dc7815baa49465c6ec4de9b3e13cf
Aljosa Urbas <aljosaurbas@gmail.com>
2023-09-15 14:09:50 +0200

    fix: fixed lint

--- ajna/v2/views/wallets.py
+++ ajna/v2/views/wallets.py
@@ -4,7 +4,6 @@ from rest_framework import status
 from rest_framework.response import Response
 
 from ajna.utils.db import fetch_one
-
 from ajna.utils.views import BaseChainView, RawSQLPaginatedChainView
 
 
