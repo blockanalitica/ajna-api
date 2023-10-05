6fd9c69dd3e7d6e25954161e51ba169c181e2dcf
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2023-10-05 15:58:06 +0200

    fix: management commant

--- ajna/management/commands/clear_v2_event_related_tables.py
+++ ajna/management/commands/clear_v2_event_related_tables.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.core.management.base import BaseCommand
 
 from ajna.v2.ethereum.chain import Ethereum
@@ -8,13 +9,19 @@ class Command(BaseCommand):
     def _clear_models(self, chain):
         chain.pool_event.objects.all().delete()
 
+        cache_key = "fetch_and_save_events_for_all_pools.{}.last_block_number".format(
+            chain.unique_key
+        )
+
+        cache.delete(cache_key)
+
         chain.wallet_bucket_state.objects.all().delete()
         chain.bucket_state.objects.all().delete()
         chain.bucket.objects.all().delete()
 
         chain.wallet_position.objects.all().delete()
         chain.current_wallet_position.objects.all().delete()
-        chain.walet.objects.all().delete()
+        chain.wallet.objects.all().delete()
 
     def handle(self, *args, **options):
         goerli = Goerli()
