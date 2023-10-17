70e679704624a402ad4a1fae5af8f36344aae8d7
Aljosa Urbas <aljosaurbas@gmail.com>
2023-10-17 09:40:06 +0200

    feat: added eoa field to Wallet model

---
+++ ajna/management/commands/populate_v2_wallet_eoa.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from django.core.management.base import BaseCommand
+
+from ajna.v2.ethereum.models import V2EthereumWallet
+from ajna.v2.goerli.models import V2GoerliPriceFeed
+from ajna.v2.ethereum.chain import Ethereum
+from ajna.v2.goerli.chain import Goerli
+
+
+class Command(BaseCommand):
+    def _populate_eth(self, chain):
+        wallets = chain.wallet.objects.filter(eoa__isnull=True)
+
+        for wallet in wallets:
+            wallet.eoa = chain.get_eoa(wallet.address)
+            wallet.save()
+
+    def handle(self, *args, **options):
+        chain = Ethereum()
+        self._populate_eth(chain)
+        chain = Goerli()
+        self._populate_eth(chain)
