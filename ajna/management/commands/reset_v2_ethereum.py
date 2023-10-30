b16fdc904cc16251506947f506d692f29e8fe299
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2023-10-30 09:49:16 +0100

    fix: cleanup obsolete scripts, added reset for v2

--- ajna/management/commands/reset_v2_ethereum.py
+++ ajna/management/commands/reset_v2_ethereum.py
@@ -1,11 +1,18 @@
+from django.core.cache import cache
 from django.core.management.base import BaseCommand
 from django.db import connection
 
-from ajna.v1.goerli.chain import MODEL_MAP
+from ajna.v2.ethereum.chain import MODEL_MAP, Ethereum
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
+        chain = Ethereum()
+
+        cache_key = "fetch_and_save_events_for_all_pools.{}.last_block_number".format(
+            chain.unique_key
+        )
+        cache.delete(cache_key)
         for key, model in MODEL_MAP.items():
             if key == "price_feed":
                 self.stdout.write("Skipping price_feed model: {}".format(model))
