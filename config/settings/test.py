5caa97acd5c88e6d4db898ad863581f6542894b3
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2024-01-22 10:00:42 +0100

    fix: erc20 pool created new logic

--- config/settings/test.py
+++ config/settings/test.py
@@ -14,6 +14,16 @@ SUBGRAPH_ENDPOINT_GOERLI = "http://localhost:8000/subgraph/"
 
 SUBGRAPH_ENDPOINT_GOERLI = "http://subgraph:8000/goerli/"
 SUBGRAPH_ENDPOINT_MAINNET = "http://subgraph:8000/mainnet/"
+
 GOERLI_NODE = "http://eth_node/goerli"
 ETHEREUM_NODE = "http://eth_node/mainnet"
+BASE_NODE = "http://eth_node/base"
+ARBITRUM_NODE = "http://eth_node/arbitrum"
+POLYGON_NODE = "http://eth_node/polygon"
+OPTIMISM_NODE = "http://eth_node/optimism"
+
 ETHERSCAN_API_KEY = "no-key"
+ARBISCAN_API_KEY = "no-key"
+BASESCAN_API_KEY = "no-key"
+OPTIMISTIC_ETHERSCAN_API_KEY = "no-key"
+POLYGONSCAN_API_KEY = "no-key"
