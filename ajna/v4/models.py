8546d6d0ffeec6a6af2fa00e8abcd4099ba0ff17
Tomaz Sifrer <tomazz.sifrer@gmail.com>
2024-05-21 11:04:09 +0200

    feat: Added Filecoin chain

--- ajna/v4/models.py
+++ ajna/v4/models.py
@@ -2,6 +2,7 @@ from ajna.v4.arbitrum.models import *  # noqa: F403
 from ajna.v4.base.models import *  # noqa: F403
 from ajna.v4.blast.models import *  # noqa: F403
 from ajna.v4.ethereum.models import *  # noqa: F403
+from ajna.v4.filecoin.models import *  # noqa: F403
 from ajna.v4.gnosis.models import *  # noqa: F403
 from ajna.v4.optimism.models import *  # noqa: F403
 from ajna.v4.polygon.models import *  # noqa: F403
