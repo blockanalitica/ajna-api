cf7ea1353a32d1a9ded651a4ce4e18f070cd5bba
tsifrer <3967564+tsifrer@users.noreply.github.com>
2026-07-11 20:20:56 +1200

    fix: disable rari

--- config/settings/base.py
+++ config/settings/base.py
@@ -136,7 +136,7 @@ CELERY_IMPORTS = [
     "ajna.v4.blast.tasks",
     "ajna.v4.gnosis.tasks",
     "ajna.v4.mode.tasks",
-    "ajna.v4.rari.tasks",
+    # "ajna.v4.rari.tasks",
     "ajna.v4.avalanche.tasks",
     "ajna.v4.linea.tasks",
     "ajna.v4.hemi.tasks",
